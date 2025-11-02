4fa4f5ec319cc4f9f3a5251b160a206a02f28e71
HaaaamBurger <gemasclashes@gmail.com>
2025-11-02 15:08:58 +0200

    cors-handling

---
+++ src/main/java/com/vendo/user_service/common/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.vendo.user_service.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${server.host}")
+    private String SERVER_HOST;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("https://%s".formatted(SERVER_HOST))
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
