2532deca49147b77c11a236b68f28a208f191f7a
HaaaamBurger <gemasclashes@gmail.com>
2025-11-04 23:16:50 +0200

    cors setting

---
+++ src/main/java/com/vendo/user_service/common/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.vendo.user_service.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({"dev", "prod"})
+public class OpenApiConfig {
+
+    @Value("${server.url}")
+    private String SERVER_URL;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url(SERVER_URL));
+    }
+}
