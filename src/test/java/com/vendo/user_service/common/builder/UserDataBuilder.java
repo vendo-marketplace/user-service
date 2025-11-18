d5bfda45408c36b518b77542e63b6ae8d545ab94
vlcdck <pridatchenkovladislav@gmail.com>
2025-11-19 00:37:57 +0200

    - Add tests for UserService & UserController

--- src/test/java/com/vendo/user_service/common/builder/UserDataBuilder.java
+++ src/test/java/com/vendo/user_service/common/builder/UserDataBuilder.java
@@ -4,16 +4,38 @@ import com.vendo.domain.user.common.type.ProviderType;
 import com.vendo.domain.user.common.type.UserStatus;
 import com.vendo.user_service.common.type.UserRole;
 import com.vendo.user_service.model.User;
+import com.vendo.user_service.web.dto.UserProfileResponse;
+
+import java.time.Instant;
+import java.time.LocalDate;
 
 public class UserDataBuilder {
 
     public static User.UserBuilder buildUserWithRequiredFields() {
         return User.builder()
+                .id("1")
                 .email("test@gmail.com")
                 .password("Qwerty1234@")
                 .role(UserRole.USER)
                 .providerType(ProviderType.LOCAL)
-                .status(UserStatus.INCOMPLETE);
+                .status(UserStatus.INCOMPLETE)
+                .birthDate(LocalDate.of(1990, 1, 1))
+                .fullName("John Doe")
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now());
     }
 
+    public static UserProfileResponse buildUserProfileResponse(User user) {
+        return new UserProfileResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getRole(),
+                user.getStatus(),
+                user.getProviderType(),
+                user.getBirthDate(),
+                user.getFullName(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
 }
