3fe01dafebee3d4827de69fe7c6c7b671c374274
vlcdck <pridatchenkovladislav@gmail.com>
2025-11-28 21:39:15 +0200

    add mapper config

--- src/main/java/com/vendo/user_service/service/user/common/mapper/UserMapper.java
+++ src/main/java/com/vendo/user_service/service/user/common/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package com.vendo.user_service.service.user.common.mapper;
 
+import com.vendo.user_service.common.config.MapStructConfig;
 import com.vendo.user_service.model.User;
 import com.vendo.user_service.web.dto.UserProfileResponse;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(config = MapStructConfig.class)
 public interface UserMapper {
 
     UserProfileResponse toUserProfileResponse(User user);
