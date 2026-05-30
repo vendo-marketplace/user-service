6cfb7a68ae2d496d9f9dff9fd2233de603404961
HaaaamBurger <gemasclashes@gmail.com>
2026-05-30 17:44:25 +0300

    kan-42: invalid content type fix

--- src/main/java/com/vendo/user_service/adapter/user/in/exception/UserExceptionHandler.java
+++ src/main/java/com/vendo/user_service/adapter/user/in/exception/UserExceptionHandler.java
@@ -1,14 +1,17 @@
-package com.vendo.user_service.adapter.user.out.exception;
+package com.vendo.user_service.adapter.user.in.exception;
 
 import com.vendo.security_lib.exception.response.ExceptionResponse;
 import com.vendo.user_lib.exception.*;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class UserExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
